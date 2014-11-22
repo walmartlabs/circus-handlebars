cc0d8f5cf4942f6584de1cee23edc3ef7c3cee35
kpdecker <kpdecker@gmail.com>
2014-11-22 14:24:20 -0600

    Implement conditional compilation
    Adds support for hybrid and SSJS builds.

--- test/fixtures/stylus.js
+++ test/fixtures/stylus.js
@@ -1,2 +1,6 @@
 require.css('./stylus.styl');
 require.css('./css2.css');
+
+if (!$serverSide) {
+  require('./router1');
+}
--- test/integration.js
+++ test/integration.js
@@ -36,6 +36,8 @@ describe('loader integration', function() {
   describe('#config', function() {
     it('should extend config', function() {
       var config = Pack.config({
+        serverSide: true,
+        hybrid: true,
         module: {
           loaders: [2]
         },
@@ -44,14 +46,30 @@ describe('loader integration', function() {
         },
         plugins: [1]
       });
+      expect(config.length).to.equal(6);
+
+      config = config[0];
       expect(config.module.loaders.length).to.equal(3);
       expect(config.module.loaders[2]).to.equal(2);
       expect(config.resolve).to.eql({
         modulesDirectories: ['web_modules', 'node_modules', 'bower_components'],
         bar: 'baz'
       });
-      expect(config.plugins.length).to.equal(3);
-      expect(config.plugins[2]).to.equal(1);
+      expect(config.plugins.length).to.equal(4);
+      expect(config.plugins[3]).to.equal(1);
+    });
+
+    it('should provide server-side chunk names', function() {
+      var config = Pack.config({
+        serverSide: true,
+        output: {
+          chunkFilename: 'foo.js'
+        }
+      });
+      expect(config.length).to.equal(2);
+
+      config = config[1];
+      expect(config.output.chunkFilename).to.equal('foo-server.js');
     });
   });
 
@@ -69,8 +87,9 @@ describe('loader integration', function() {
       }
     }), function(err, status) {
       expect(err).to.not.exist;
-      expect(status.compilation.errors).to.be.empty;
-      expect(status.compilation.warnings).to.be.empty;
+      var compilation = status.stats[0].compilation;
+      expect(compilation.errors).to.be.empty;
+      expect(compilation.warnings).to.be.empty;
 
       runPhantom(function(err, loaded) {
         // Opposite order as the loader injects into the top of head
@@ -100,8 +119,10 @@ describe('loader integration', function() {
       }
     }), function(err, status) {
       expect(err).to.not.exist;
-      expect(status.compilation.errors).to.be.empty;
-      expect(status.compilation.warnings).to.be.empty;
+
+      var compilation = status.stats[0].compilation;
+      expect(compilation.errors).to.be.empty;
+      expect(compilation.warnings).to.be.empty;
 
       var pack = JSON.parse(fs.readFileSync(outputDir + '/circus.json').toString());
       expect(_.pluck(pack.modules, 'name').sort()).to.eql([
@@ -125,6 +146,41 @@ describe('loader integration', function() {
       });
     });
   });
+
+
+  it('should build server files', function(done) {
+    var entry = path.resolve(__dirname + '/fixtures/stylus.js');
+
+    outputDir = 'tmp/';
+
+    webpack(Pack.config({
+      entry: entry,
+      serverSide: true,
+      output: {
+        path: outputDir
+      }
+    }), function(err, status) {
+      expect(err).to.not.exist;
+
+      var compilation = status.stats[0].compilation;
+      expect(compilation.errors).to.be.empty;
+      expect(compilation.warnings).to.be.empty;
+
+      expect(Object.keys(compilation.assets)).to.eql(['bundle.js', '0.bundle.css', 'circus.json', 'bundle.js.map']);
+
+      // Verify the actual css content
+      var output = fs.readFileSync(outputDir + '/bundle.js').toString();
+      expect(output).to.match(/if \(true\)/);
+      expect(output).to.match(/Zeus\.router/);
+
+      output = fs.readFileSync(outputDir + '/bundle-server.js').toString();
+      expect(output).to.match(/if \(false\)/);
+      expect(output).to.not.match(/Zeus\.router/);
+
+      done();
+    });
+  });
+
   it('should compile stylus into external css files', function(done) {
     var entry = path.resolve(__dirname + '/fixtures/stylus.js');
 
@@ -138,19 +194,68 @@ describe('loader integration', function() {
       }
     }), function(err, status) {
       expect(err).to.not.exist;
-      expect(status.compilation.errors).to.be.empty;
-      expect(status.compilation.warnings).to.be.empty;
 
-      expect(Object.keys(status.compilation.assets)).to.eql(['bundle.js', '0.bundle.css', 'circus.json', 'bundle.js.map']);
+      var compilation = status.stats[0].compilation;
+      expect(compilation.errors).to.be.empty;
+      expect(compilation.warnings).to.be.empty;
+
+      expect(Object.keys(compilation.assets)).to.eql(['bundle.js', '0.bundle.css', 'circus.json', 'bundle.js.map']);
 
       // Verify the actual css content
       var output = fs.readFileSync(outputDir + '/0.bundle.css').toString();
       expect(output).to.match(/\.foo\s*\{/);
       expect(output).to.match(/\.baz\s*\{/);
+      expect(output).to.not.match(/\.browser\s*\{/);
+      expect(output).to.not.match(/\.android\s*\{/);
+      expect(output).to.not.match(/\.ios\s*\{/);
 
       done();
     });
   });
+  it('should build hybrid style files', function(done) {
+    var entry = path.resolve(__dirname + '/fixtures/stylus.js');
+
+    webpack(Pack.config({
+      entry: entry,
+      hybrid: true,
+      output: {
+        path: outputDir
+      }
+    }), function(err, status) {
+      expect(err).to.not.exist;
+
+      var compilation = status.stats[0].compilation;
+      expect(compilation.errors).to.be.empty;
+      expect(compilation.warnings).to.be.empty;
+
+      expect(Object.keys(compilation.assets)).to.eql(['bundle.js', '0.bundle.css', 'circus.json', 'bundle.js.map']);
+
+      // Verify the actual css content
+      var output = fs.readFileSync(outputDir + '/browser/0.bundle.css').toString();
+      expect(output).to.match(/\.foo\s*\{/);
+      expect(output).to.match(/\.baz\s*\{/);
+      expect(output).to.match(/\.browser\s*\{/);
+      expect(output).to.not.match(/\.android\s*\{/);
+      expect(output).to.not.match(/\.ios\s*\{/);
+
+      output = fs.readFileSync(outputDir + '/ios/0.bundle.css').toString();
+      expect(output).to.match(/\.foo\s*\{/);
+      expect(output).to.match(/\.baz\s*\{/);
+      expect(output).to.not.match(/\.browser\s*\{/);
+      expect(output).to.not.match(/\.android\s*\{/);
+      expect(output).to.match(/\.ios\s*\{/);
+
+      output = fs.readFileSync(outputDir + '/android/0.bundle.css').toString();
+      expect(output).to.match(/\.foo\s*\{/);
+      expect(output).to.match(/\.baz\s*\{/);
+      expect(output).to.not.match(/\.browser\s*\{/);
+      expect(output).to.match(/\.android\s*\{/);
+      expect(output).to.not.match(/\.ios\s*\{/);
+
+      done();
+    });
+  });
+
   it('should precompile handlebars templates', function(done) {
     var entry = path.resolve(__dirname + '/fixtures/handlebars-render.js');
 
@@ -164,8 +269,10 @@ describe('loader integration', function() {
       }
     }), function(err, status) {
       expect(err).to.not.exist;
-      expect(status.compilation.errors).to.be.empty;
-      expect(status.compilation.warnings).to.be.empty;
+
+      var compilation = status.stats[0].compilation;
+      expect(compilation.errors).to.be.empty;
+      expect(compilation.warnings).to.be.empty;
 
       runPhantom(function(err, loaded) {
         expect(loaded.log).to.eql([
