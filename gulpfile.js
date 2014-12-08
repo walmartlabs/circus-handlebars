543cd55ca95b377adf46899236d93d6033a523c8
kpdecker <kpdecker@gmail.com>
2014-12-08 08:38:33 -0800

    Export gulp tasks as more formal API

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,9 @@
 */
 
 var gulp = require('gulp'),
-    requireDir = require('require-dir'),
     config = require('./gulp/config');
 
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
+require('./gulp');
 
 gulp.task('default', ['lint']);
 
